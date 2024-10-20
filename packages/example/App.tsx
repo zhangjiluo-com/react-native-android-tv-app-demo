dc31cbe13ffe509eeb959394d2f3e7ed07a05260
zhangjiluo.com <zhangjiluo.com@gmail.com>
2024-10-20 21:04:53 +0800

    1

--- packages/example/App.tsx
+++ packages/example/App.tsx
@@ -19,12 +19,12 @@ import { NonVirtualizedGridPage } from './src/pages/NonVirtualizedGridPage';
 import { GridWithLongNodesPage } from './src/pages/GridWithLongNodesPage';
 import { useTVPanEvent } from './src/components/PanEvent/useTVPanEvent';
 import { SpatialNavigationDeviceTypeProvider } from '../lib/src/spatial-navigation/context/DeviceContext';
-import { ListWithVariableSize } from './src/pages/ListWithVariableSize';
-import { AsynchronousContent } from './src/pages/AsynchronousContent';
-import * as server from 'expo-http-server';
-import { useEffect, useState } from 'react';
-import { NetworkInfo } from 'react-native-network-info';
-import { WebView } from 'react-native-webview';
+// import { ListWithVariableSize } from './src/pages/ListWithVariableSize';
+// import { AsynchronousContent } from './src/pages/AsynchronousContent';
+// import * as server from 'expo-http-server';
+// import { useEffect, useState } from 'react';
+// import { NetworkInfo } from 'react-native-network-info';
+// import { WebView } from 'react-native-webview';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -71,84 +71,10 @@ const TabNavigator = () => {
   );
 };
 
-const html = (title: string) => `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  </head>
-  <body>
-    <h1>${title}</h1>
-    <form action="/play" method="post">
-      <button type="submit">发送播放信号</button>
-    </form>
-    <form action="/pause" method="post">
-      <button type="submit">发送暂停信号</button>
-    </form>
-  </body>
-</html>`;
 function App() {
   useTVPanEvent();
   const { height, width } = useWindowDimensions();
   const areFontsLoaded = useFonts();
-
-  const [lastCalled, setLastCalled] = useState<number | undefined>();
-  const [lastCalled2, setLastCalled2] = useState<string | undefined>('null');
-
-  const [webUrl, setWebUrl] = useState<string | undefined>();
-
-  useEffect(() => {
-    server.setup(9666, async (event: server.StatusEvent) => {
-      if (event.status === 'ERROR') {
-        // there was an error...
-      } else {
-        // server was STARTED, PAUSED, RESUMED or STOPPED
-      }
-      console.log(event.status, event.message);
-
-      const ipv4 = await NetworkInfo.getIPV4Address();
-      console.log(ipv4);
-      const url = `http://${ipv4}:9666/`;
-      setWebUrl(url);
-    });
-
-    server.route('/', 'GET', async (request) => {
-      console.log('Request', '/', 'GET', request);
-      setLastCalled(Date.now());
-      return {
-        statusCode: 200,
-        statusDescription: 'OK - CUSTOM STATUS',
-        contentType: 'text/html',
-        body: html(''),
-      };
-    });
-    server.route('/play', 'POST', async (request) => {
-      console.log('Request', '/play', 'POST', request);
-      setLastCalled(Date.now());
-      setLastCalled2('获取到播放信号');
-      return {
-        statusCode: 200,
-        statusDescription: 'OK - CUSTOM STATUS',
-        contentType: 'text/html',
-        body: html('获取到播放信号'),
-      };
-    });
-    server.route('/pause', 'POST', async (request) => {
-      console.log('Request', '/pause', 'POST', request);
-      setLastCalled(Date.now());
-      setLastCalled2('获取到暂停信号');
-      return {
-        statusCode: 200,
-        statusDescription: 'OK - CUSTOM STATUS',
-        contentType: 'text/html',
-        body: html('获取到暂停信号'),
-      };
-    });
-    server.start();
-    return () => {
-      server.stop();
-    };
-  }, []);
 
   if (!areFontsLoaded) {
     return null;
@@ -159,7 +85,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <SpatialNavigationDeviceTypeProvider>
           <Container width={width} height={height}>
-            {/* <Stack.Navigator
+            <Stack.Navigator
               screenOptions={{
                 headerShown: false,
                 contentStyle: {
@@ -170,15 +96,15 @@ function App() {
             >
               <Stack.Screen name="TabNavigator" component={TabNavigator} />
               <Stack.Screen name="ProgramDetail" component={ProgramDetail} />
-            </Stack.Navigator> */}
-            <View>
+            </Stack.Navigator>
+            {/* <View>
               <Text style={{ color: 'red' }}>
                 {webUrl}
                 {'\n'}
               </Text>
               <Text style={{ color: 'red' }}>{lastCalled2}</Text>
             </View>
-            {webUrl && <WebView source={{ uri: webUrl }}></WebView>}
+            {webUrl && <WebView source={{ uri: webUrl }}></WebView>} */}
           </Container>
         </SpatialNavigationDeviceTypeProvider>
       </ThemeProvider>
---
+++ packages/example/src/hooks/use-server.ts
@@ -0,0 +1,93 @@
+import { route, start, setup, stop, Status } from 'expo-http-server';
+import mitt, { Emitter } from 'mitt';
+import { useEffect } from 'react';
+import { NetworkInfo } from 'react-native-network-info';
+
+const html = (title: string) => `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+  <body>
+    <h1>${title}</h1>
+    <form action="/play" method="post">
+      <button type="submit">发送播放信号</button>
+    </form>
+    <form action="/pause" method="post">
+      <button type="submit">发送暂停信号</button>
+    </form>
+  </body>
+</html>`;
+
+let serverClient: {
+  stop(): void;
+  status: Status;
+  emitter: Emitter<{
+    play: undefined;
+    pause: undefined;
+  }>;
+  port: number;
+  url: string;
+};
+
+function startServer() {
+  if (!serverClient) {
+    serverClient = {
+      stop,
+      status: 'STOPPED',
+      emitter: mitt(),
+      port: 9666,
+      url: '',
+    };
+  }
+  setup(serverClient.port, async function (params) {
+    serverClient.status = params.status;
+    const ipv4 = await NetworkInfo.getIPV4Address();
+    serverClient.url = `http://${ipv4}:${serverClient.port}/`;
+  });
+
+  //   route('/api', 'POST', async function () {
+  //     return {
+  //       statusCode: 200,
+  //       contentType: 'text/html',
+  //       body: html(''),
+  //     };
+  //   });
+  route('/api/play', 'POST', async () => {
+    serverClient.emitter.emit('play');
+    return {
+      statusCode: 200,
+      contentType: 'text/html',
+      body: html(''),
+    };
+  });
+  route('/api/pause', 'POST', async () => {
+    serverClient.emitter.emit('pause');
+    return {
+      statusCode: 200,
+      contentType: 'text/html',
+      body: html(''),
+    };
+  });
+
+  start();
+  return serverClient;
+}
+
+export function useServer(listen?: { onPlay?(): void; onPause?(): void }) {
+  const server = startServer();
+  const onPlay = listen?.onPlay;
+  const onPause = listen?.onPause;
+
+  useEffect(() => {
+    onPlay && server.emitter.on('play', onPlay);
+    onPause && server.emitter.on('pause', onPause);
+
+    return () => {
+      onPlay && server.emitter.off('play', onPlay);
+      onPause && server.emitter.on('pause', onPause);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [listen]);
+}
